60a420ba0425a0c94fc21a4d33ee15b158b35733
Sean M. Collins <sean@coreitpro.com>
2013-10-02 13:04:42 -0400

    Manage Apache config via puppet

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,7 @@
 include apt
+include apache
 
-$www_packages = ['php5','sqlite']
+$www_packages = ['php5','sqlite3','php5-sqlite']
 
 
 
@@ -11,4 +12,29 @@ exec { "apt-get update":
 package { $www_packages: 
 		ensure => 'installed',
 	  	require  => Exec['apt-get update'],
+}
+
+
+exec { "mod_rewrite":
+    command => "/usr/sbin/a2enmod rewrite",
+}
+
+exec { "reload":
+       command=> "/usr/sbin/apachectl graceful",
+       require=> Exec['mod_rewrite']
+}
+
+exec { "sqlite setup":
+       command => "/usr/bin/sqlite3 /var/www/data.db < /var/www/db/schema.sqlite",
+       require=> Exec['reload']
+}
+
+apache::vhost { '*:80':
+      docroot => '/var/www/',
+      directories => [ { path => '/var/www', allow_override => ['ALL'] } ],
+      require=> Exec['reload']
+}
+
+file { "/etc/apache2/mods-available/":
+  ensure => "present",
 }
